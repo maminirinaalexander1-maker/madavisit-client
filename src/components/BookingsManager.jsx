7a027bb750800f26f5c422da6bb92905448c786d
maminirinaalexander1-maker <maminirinaalexander1@gmail.com>
2026-03-19 12:09:49 +0300

    Fix: correction chemin import firebase dans StatsOverview

--- src/components/BookingsManager.jsx
+++ src/components/BookingsManager.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { db } from "../../firebase/config";
+import { db } from "../firebase/config";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 
 const BookingsManager = () => {
